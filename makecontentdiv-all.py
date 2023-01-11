0776e417cd62deb1c194c54c2da536e928c9739f
Alan Jones <jones.alan@hotmail.co.uk>
2023-01-11 16:53:36 +0100

    update

---
+++ makecontentdiv-all.py
@@ -0,0 +1,25 @@
+import markdown
+
+names = ["sfs-Intro",
+        "sfs-Installation",
+        "sfs-editors",
+        "sfs-text",
+        "sfs-media",
+        "sfs-data-intro",
+        "sfs-data-text",
+        "sfs-data-stcharts",
+        "sfs-data-pyplot",
+        "sfs-data-altair",
+        "sfs-data-plotly",
+        "sfs-data-bokeh",
+        "sfs-data-vega-lite",
+        "sfs-dashboard-layouts"]
+
+for name in names:
+    print(name)
+    with open(f'./{name}.md', 'r') as reader:
+        md = reader.read()
+    html = markdown.markdown(md, extensions=['fenced_code'])
+    o = open(f'./{name}.div','w')
+    o.write(html)
+    o.close()
